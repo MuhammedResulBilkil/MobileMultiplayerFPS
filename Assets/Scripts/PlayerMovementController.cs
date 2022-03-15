40783cd38f0ad04814fa226c1141abb6418a4b91
Resul <garnnet_221@hotmail.com>
2022-03-15 11:38:37 +0300

    Character Animations Part-2: Blend Trees

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -6,11 +6,22 @@ using UnityStandardAssets.Characters.FirstPerson;
 
 public class PlayerMovementController : MonoBehaviour
 {
+    [SerializeField] private RigidbodyFirstPersonController _rigidbodyFirstPersonController;
+
     public FixedTouchField fixedTouchField;
     public Joystick joystick;
+
+    private Animator _animator;
     
-    [SerializeField] private RigidbodyFirstPersonController _rigidbodyFirstPersonController;
-    
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Running = Animator.StringToHash("Running");
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -18,5 +29,21 @@ public class PlayerMovementController : MonoBehaviour
         _rigidbodyFirstPersonController.joystickInputAxis.y = joystick.Vertical;
 
         _rigidbodyFirstPersonController.mouseLook.lookInputAxis = fixedTouchField.TouchDist;
+
+        _animator.SetFloat(Horizontal, joystick.Horizontal);
+        _animator.SetFloat(Vertical, joystick.Vertical);
+
+        if (Mathf.Abs(joystick.Horizontal) > 0.9f || Mathf.Abs(joystick.Vertical) > 0.9f)
+        {
+            //Running
+            _rigidbodyFirstPersonController.movementSettings.ForwardSpeed = 16f;
+            _animator.SetBool(Running, true);
+        }
+        else
+        {
+            //Walking
+            _rigidbodyFirstPersonController.movementSettings.ForwardSpeed = 8f;
+            _animator.SetBool(Running, false);
+        }
     }
 }
